e598157e8eac55f26d7e4d94faf8eb7b4797471c
Francisco Cuauhtemoc Castañeda Rovira <31245060+dragaus@users.noreply.github.com>
2023-11-18 14:57:51 -0600

    Destroy Ship

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Nave : MonoBehaviour
 {
     Rigidbody rigi;
+    public LayerMask queEsMuerte;
     public float fuerzaNave = 1000;
     public float fuerzaRotacion = 30;
 
@@ -19,17 +21,28 @@ public class Nave : MonoBehaviour
     {
         //transform.Rotate(Vector3.right * 2);
 
-        if(Input.GetKey
-        
-        
-        (KeyCode.D))
+        if(Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.forward * -fuerzaRotacion * Time.deltaTime);    
         }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * fuerzaRotacion * Time.deltaTime);    
+        }
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    rigi.AddForce(Vector3.up * fuerzaNave);
-        //}
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Z))
+        {
+           rigi.AddForce(transform.up * fuerzaNave);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(( queEsMuerte & (1 << collision.collider.gameObject.layer)) != 0)
+        {
+            Debug.Log(collision.collider.gameObject.layer);
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
