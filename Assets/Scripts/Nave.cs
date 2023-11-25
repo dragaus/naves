e4c6b28adb8a8b5b923d9fd65e688751e3ba2a63
Francisco Castañeda <dragalichi@gmail.com>
2023-11-25 07:25:29 -0600

    Upload

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,11 +10,14 @@ public class Nave : MonoBehaviour
     public LayerMask queEsMuerte;
     public float fuerzaNave = 1000;
     public float fuerzaRotacion = 30;
+    public bool estaDestruida = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rigi = GetComponent<Rigidbody>();
+
+        //int numeroHijos = transform.GetChild(0).childCount;
     }
 
     // Update is called once per frame
@@ -38,11 +42,66 @@ public class Nave : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        if (estaDestruida) return;
+
         if(( queEsMuerte & (1 << collision.collider.gameObject.layer)) != 0)
         {
-            Debug.Log(collision.collider.gameObject.layer);
-            Destroy(gameObject);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            StartCoroutine(DestruirNave());
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("¡Gane!");
+        }
+    }
+
+    IEnumerator DestruirNave()
+    {
+        Debug.Log("Hola");
+        estaDestruida = true;
+        //Es vamos a esperar exactamente 1 segundo antes de avanzar a la siguiente instruccion
+        VolarComponentes();
+        yield return new WaitForSeconds(10f);
+        Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Adios");
+    }
+
+    void VolarComponentes() 
+    {
+        int numeroHijos = transform.childCount;
+
+        for (int i = 0; i < numeroHijos; i++)
+        {
+            Debug.Log(i);
+            //Destroy(rigi);
+            GameObject hijo = transform.GetChild(i).gameObject;
+
+            //(1,1,1) * 2
+            Rigidbody hijoRigi = hijo.AddComponent<Rigidbody>();
+
+            int x = Random.Range(-1000, 1000);
+            int y = Random.Range(-1000, 1000);
+            int z = Random.Range(-1000, 1000);
+
+            Vector3 direccion = new Vector3(x, y, z);
+
+            hijoRigi.AddForce(direccion);
+
+            //ctr + k, ctr + c esto comenta
+            //ctr + k, ctr + u quita comentario
+            int xR = Random.Range(0, 360);
+            int yR = Random.Range(0, 360);
+            int zR = Random.Range(0, 360);
+
+            Vector3 rotacion = new Vector3(xR, yR, zR);
+
+            transform.rotation = Quaternion.Euler(rotacion);
+
+            hijoRigi.AddForceAtPosition(direccion, Vector3.down);
         }
     }
 }
